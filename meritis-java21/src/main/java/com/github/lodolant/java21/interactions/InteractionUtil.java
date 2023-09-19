c9c92e79246b35dab2f2e21a7f638be4196b4cc1
Lionel <Lionel@DESKTOP-F0JBOLJ.home>
2023-09-19 03:12:58 +0200

    Unnamed Patterns and variables
    Signed-off-by: Lionel <Lionel@DESKTOP-F0JBOLJ.home>
    Change-Id: I26b3d5590bf0695b7fb98bf1367902664ef136e2

--- meritis-java21/src/main/java/com/github/lodolant/java21/interactions/InteractionUtil.java
+++ meritis-java21/src/main/java/com/github/lodolant/java21/interactions/InteractionUtil.java
@@ -19,7 +19,7 @@ public class InteractionUtil {
 		case null -> new InteractionClientCheck();
 		case Ingredient ingredient when ingredient.isFlagged() -> new InteractionClientCheckConsumption(ingredient);
 		case Ingredient ingredient -> new InteractionClientTakeOrder(ingredient);
-		case RecipeIngredient(Ingredient ingredient, int quantity) -> new InteractionClientCheckCalories(quantity);
+		case RecipeIngredient(Ingredient _, int quantity) -> new InteractionClientCheckCalories(quantity);
 		default -> throw new ServiceException(HELP.formatted(interactionTarget.getClass().getSimpleName()));
 		};
 	}
@@ -27,11 +27,11 @@ public class InteractionUtil {
 	public static InteractionKitchen interactWithKitchen(InteractionClient interactionTarget) {
 		LOGGER.info("Interacting with kitchen: {}", interactionTarget);
 		return switch (interactionTarget) {
-		case InteractionClientTakeOrder takeOrder -> InteractionKitchen.GIVE_COMMAND;
+		case InteractionClientTakeOrder _ -> InteractionKitchen.GIVE_COMMAND;
 		case InteractionClientCheckConsumption checkConsumtion when checkConsumtion.getIngredient().isFlagged() -> InteractionKitchen.CHECK_ALLERGENES;
-		case InteractionClientCheckConsumption checkConsumtion -> InteractionKitchen.COMMAND_MORE;
-		case InteractionClientCheck checkClient -> throw new ServiceException(HELP.formatted(interactionTarget.getClass().getSimpleName()));
-		case InteractionClientCheckCalories checkClient -> throw new ServiceException(HELP.formatted(interactionTarget.getClass().getSimpleName()));
+		case InteractionClientCheckConsumption _ -> InteractionKitchen.COMMAND_MORE;
+		case InteractionClientCheck _ -> throw new ServiceException(HELP.formatted(interactionTarget.getClass().getSimpleName()));
+		case InteractionClientCheckCalories _ -> throw new ServiceException(HELP.formatted(interactionTarget.getClass().getSimpleName()));
 		};
 	}
 }
